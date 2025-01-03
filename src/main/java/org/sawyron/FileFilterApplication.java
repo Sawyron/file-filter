07654e7a02588bca23a380a2e7e6f01664abff79
Sawyron <s4pakvv@gmail.com>
2025-01-03 15:32:58 +0700

    add FileFilterApplication

---
+++ src/main/java/org/sawyron/FileFilterApplication.java
@@ -0,0 +1,11 @@
+package org.sawyron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFilterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileFilterApplication.class, args);
+    }
+}
--- src/main/java/org/sawyron/Main.java
+++
@@ -1,7 +0,0 @@
-package org.sawyron;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
